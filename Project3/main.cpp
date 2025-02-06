37b97d72099506a471e45f21e542f75ff0d9b8a9
Cheng, YIk Hei <s.am36505@gmail.com>
2025-02-06 16:38:50 -0500

    加入專案檔案。

---
+++ Project3/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+int main()
+{
+    /*sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();*/
+
+    std::cout << "Hello form SFML!" << "\n";
+    system("pause");
+    return 0;
+    }
